f3a3a7671a0450f555461ca7025df8f7e63acf40
Roman Vayvod <littlewhywhat@gmail.com>
2014-02-12 15:04:26 +0400

    create IEvent

---
+++ CalendarContentHelper/src/IEvent.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+
+interface IEvent 
+{
+	public int getID();
+	public int setID();
+	public Date getDTSTART();
+	public Date setDTSTART();
+	public String setDURATION();
+	public String getDURATION();
+	public String setTitle();
+	public String getTitle();
+}
