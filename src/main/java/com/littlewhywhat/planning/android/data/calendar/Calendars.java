501d27958dcfd9467e2e210a4c96cf894430d00d
littlewhywhat <littlewhywhat@gmail.com>
2015-05-28 21:10:50 +0300

    renamed CalendarsHelper to Calendars

--- src/main/java/com/littlewhywhat/planning/android/data/calendar/Calendars.java
+++ src/main/java/com/littlewhywhat/planning/android/data/calendar/Calendars.java
@@ -1,8 +1,9 @@
 package com.littlewhywhat.planning.android.data.calendar;
 
 import android.provider.CalendarContract;
+import android.net.Uri;
 
-class CalendarsHelper {
+class Calendars {
 	public static final String selection = "((" + CalendarContract.Calendars.VISIBLE + " = ?) AND (" 
             + CalendarContract.Calendars.SYNC_EVENTS + " = ?)) ";
 	public static final String[] SelectionArgs = new String[] { "1" , "1" };
@@ -10,6 +11,7 @@ class CalendarsHelper {
 		CalendarContract.Calendars._ID,                      // 0
 		CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,    // 1
 	};
-	public static final int PROJECTION_ID_INDEX = 0;
-	public static final int PROJECTION_NAME_INDEX = 1;	
+	public static final Uri CONTENT_URI = CalendarContract.Calendars.CONTENT_URI;
+	private static final int PROJECTION_ID_INDEX = 0;
+	private static final int PROJECTION_NAME_INDEX = 1;	
 }
--- src/main/java/com/littlewhywhat/planning/android/data/calendar/CalendarsLoader.java
+++ src/main/java/com/littlewhywhat/planning/android/data/calendar/CalendarsLoader.java
@@ -2,12 +2,10 @@ package com.littlewhywhat.planning.android.data.calendar;
 
 import android.content.Context;
 import android.content.CursorLoader;
-import android.provider.CalendarContract.Calendars;
 
 public class CalendarsLoader extends CursorLoader {
 
 	public CalendarsLoader(Context context) {
-		super(context, Calendars.CONTENT_URI, CalendarsHelper.CALENDAR_PROJECTION, CalendarsHelper.selection, CalendarsHelper.SelectionArgs, null);
+		super(context, Calendars.CONTENT_URI, Calendars.CALENDAR_PROJECTION, Calendars.selection, Calendars.SelectionArgs, null);
 	}
-
 }
